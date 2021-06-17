2aba8230c8bc86b1a722ac28f1f8378373410f37
Allen Goodman <allen.goodman@icloud.com>
2021-06-17 16:02:47 -0400

    example widget

--- napari_features/table.py
+++ napari_features/table.py
@@ -1,27 +1,58 @@
 import magicgui.widgets
+import napari
 import napari_plugin_engine
+import napari.layers
+import pandas
 import qtpy.QtWidgets
 
+from .generator import Generator
 
-class Table(qtpy.QtWidgets.QWidget):
-    def __init__(self, napari_viewer):
-        super(Table, self).__init__()
 
-        self.data = {
-            "col1": ["1", "2", "3", "4"],
-            "col2": ["1", "2", "1", "3"],
-            "col3": ["1", "1", "2", "1"]
-        }
+class Widget(qtpy.QtWidgets.QWidget):
+    def __init__(self, napari_viewer: napari.Viewer):
+        self.viewer = napari_viewer
 
-        table = magicgui.widgets.Table(value=self.data)
+        images = []
+
+        for layer in self.viewer.layers:
+            if isinstance(layer, napari.layers.Image):
+                images.append(layer)
+
+        if images:
+            self.image = images[0]
+        else:
+            return
+
+        labels = []
+
+        for layer in self.viewer.layers:
+            if isinstance(layer, napari.layers.Labels):
+                labels.append(layer)
+
+        if labels:
+            self.labels = labels[0]
+        else:
+            return
+
+        generator = Generator(self.image.data, self.labels.data)
+
+        data = [generated for generated in generator]
+
+        data = pandas.DataFrame(data, columns=generator.names)
+
+        super().__init__()
 
         self.setWindowTitle("Features")
 
-        self.setLayout(qtpy.QtWidgets.QGridLayout())
+        table = magicgui.widgets.Table(value=data.to_dict("list"))
 
-        self.layout().addWidget(table.native)
+        layout = qtpy.QtWidgets.QGridLayout()
+
+        layout.addWidget(table.native)
+
+        self.setLayout(layout)
 
 
 @napari_plugin_engine.napari_hook_implementation
 def napari_experimental_provide_dock_widget():
-    return Table
+    return Widget
