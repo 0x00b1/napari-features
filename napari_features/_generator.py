6da6e9bab3c55e48950209b64985d75ca6b3efb0
Allen Goodman <allen.goodman@icloud.com>
2021-06-29 17:25:58 -0400

    new generator

---
+++ napari_features/_generator.py
@@ -0,0 +1,31 @@
+import collections.abc
+import functools
+import typing
+
+import numpy
+
+V = typing.TypeVar("V")
+
+
+def cache(wrapped):
+    @functools.wraps(wrapped)
+    def function(generator: "Generator"):
+        name = wrapped.__name__
+
+        if not (name in generator.cache):
+            generator.cache[name] = wrapped(generator)
+
+        return generator.cache[name]
+
+    return function
+
+
+class Generator(collections.abc.Iterator):
+    cache: dict[str, object] = {}
+
+    images: dict[str, numpy.array] = {}
+
+    objects: dict[str, numpy.array] = {}
+
+    def __next__(self) -> V:
+        pass
