3ebc2b556a55995e2e61cfc3a91835ddaf861c65
Allen Goodman <allen.goodman@icloud.com>
2021-06-29 17:27:11 -0400

    new features

---
+++ napari_features/_features.py
@@ -0,0 +1,28 @@
+import napari.layers
+import napari.types
+import napari.utils.events
+import napari_plugin_engine
+
+from ._generator import Generator
+
+
+def features(viewer: napari.Viewer):
+    generator = Generator()
+
+    @viewer.layers.events.connect
+    def generate(event: napari.utils.events.event.Event):
+        if event.type not in {"changed", "inserted", "removed"}:
+            return
+
+        source = event.source[0]
+
+        if isinstance(source, napari.layers.Image):
+            generator.images[source.source] = source.data
+
+        if isinstance(source, napari.layers.Labels):
+            generator.objects[source.source] = source.data
+
+
+@napari_plugin_engine.napari_hook_implementation
+def napari_experimental_provide_function():
+    return [features]
