402801b4fa618c54a697abdff62b7bc2b23d547f
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-07-02 20:30:59 +0200

    add save and copy buttons

--- napari_features/_dock_widget.py
+++ napari_features/_dock_widget.py
@@ -14,8 +14,27 @@ class DockWidget(qtpy.QtWidgets.QWidget):
 
         self.setWindowTitle("Features")
 
+        # store data
         self.table = magicgui.widgets.Table()
 
+        self.copy_button = qtpy.QtWidgets.QPushButton("Copy to clipboard")
+
+        # operations on data
+        @self.copy_button.clicked.connect
+        def copy_trigger():
+            self.table.to_dataframe().to_clipboard()
+
+        self.save_button = qtpy.QtWidgets.QPushButton("Save as csv...")
+
+        @self.save_button.clicked.connect
+        def save_trigger():
+            filename, _ = qtpy.QtWidgets.QFileDialog.getSaveFileName(self.save_button, "Save as csv...", ".", "*.csv")
+            self.table.to_dataframe().to_csv(filename)
+
+        # build GUI
         self.setLayout(qtpy.QtWidgets.QGridLayout())
 
+        self.layout().addWidget(self.copy_button)
+        self.layout().addWidget(self.save_button)
         self.layout().addWidget(self.table.native)
+
