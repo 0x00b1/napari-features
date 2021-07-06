5f0eae2e88fb3e12d6be95ded45ada38ee04be5b
Allen Goodman <allen.goodman@icloud.com>
2021-07-06 17:41:33 -0400

    metadata

--- napari_features/_features.py
+++ napari_features/_features.py
@@ -20,6 +20,12 @@ def features(
 
     data = pandas.DataFrame([feature for feature in generator], columns=generator.columns)
 
+    data["image_name"] = image.name
+    data["image_path"] = image.source.path
+
+    data["masks_name"] = image.name
+    data["masks_path"] = image.source.path
+
     dock_widget.table.value = data
 
 
